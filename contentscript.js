cc90eb66636d226da631f10e4fe2ebf33d685019
Keith Johnson <kjcjohnson@ymail.com>
2013-09-21 04:14:14 -0400

    Try integrating gestures...

--- contentscript.js
+++ contentscript.js
@@ -2,4 +2,25 @@
 //These are injected into the webpage.
 //use Chrome extensions
 
+chrome.extension.onRequest.addListener(function(msg, sender, sendResponse) {
+    if (msg == "back") {
+	pageBack();
+    } else if (msg == "forward") {
+	pageForward();
+    } else if (msg == "refresh") {
+	pageRefresh();
+    } else if (msg == "scroll_up") {
+	scrollUp();
+    } else if (msg == "scroll_down") {
+	scrollDown();
+    } else if (msg == "scroll_left") {
+	scrollLeft();
+    } else if (msg == "scroll_right") {
+	scrollRight();
+    }
+
+    alert( msg );
+
+    sendResponse();
+});
 
